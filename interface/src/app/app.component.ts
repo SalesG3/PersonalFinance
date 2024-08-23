8743b00cf38304328d2bc65beed58a0c7674c3d2
Sales <contabilgs3@gmail.com>
2024-08-23 16:52:08 -0300

    intercafe: bank component create

--- interface/src/app/app.component.ts
+++ interface/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BankComponent } from './bank/bank.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BankComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
